e773551683f5fe68276fcb86c7556dbc3ccf253d
Jorge Leandro Lima Carvalho <90215418+JLeandrolc@users.noreply.github.com>
2022-06-08 19:44:06 +0000

    Initial commit

---
+++ migrations/20220608193805-create-livros.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('livros', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        type: Sequelize.STRING
+      },
+      autor: {
+        type: Sequelize.STRING
+      },
+      data_publicacao: {
+        type: Sequelize.DATE
+      },
+      editora: {
+        type: Sequelize.STRING
+      },
+      preco: {
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('livros');
+  }
+};
